2f9762eef0c08cdab2e125361595882fec72d36d
Ayaan Grover <ayaangrover@gmail.com>
2024-09-14 10:35:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let rooms = {
-  "General": { messages: [], members: [] } // Initialize with a default "General" room (capitalized)
+  "General": { messages: [], members: [] } // Initialize with a default "General" room
 };
 
 app.get("/", sendMessages);
